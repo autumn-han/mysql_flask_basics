879caeaf155be45b3b235fbfd734266c9636adc4
Autumn Han <autumn9894@gmail.com>
2023-05-10 19:04:22 -0500

    working on all recipes page

---
+++ belt_review/recipes/flask_app/controllers/recipes.py
@@ -0,0 +1,4 @@
+from flask_app import app
+from flask import render_template, session
+
+
--- belt_review/recipes/flask_app/controllers/users.py
+++ belt_review/recipes/flask_app/controllers/users.py
@@ -1,6 +1,24 @@
 from flask_app import app
-from flask import render_template
+from flask_app.models.user import User
+from flask import render_template, redirect, request, session
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/process/registration', methods=['POST'])
+def create_user():
+    print(request.form)
+    pw_hash = bcrypt.generate_password_hash(request.form["password"])
+    data = {
+        "first_name": request.form["fname"],
+        "last_name": request.form["lname"],
+        "email": request.form["email"],
+        "password": pw_hash
+    }
+    user_id = User.create(data)
+    session["user_id"] = user_id
+    return redirect('#')
